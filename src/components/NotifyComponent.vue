45d9bd86579a12619686f8628cb2d3751f6ce81c
Tim <tinghom@gmail.com>
2018-10-05 16:15:37 +0800

    refactor: arrayToList method

--- src/components/NotifyComponent.vue
+++ src/components/NotifyComponent.vue
@@ -1,37 +1,34 @@
 <template>
-    
+
 </template>
 
 <script>
     import Vue from 'vue';
 
     export default Vue.extend({
-        props: {
-            messageBag: {},
-        },
-        mounted () {
-            window.addEventListener('load', event => {
-                this.$notify({
-                    title: this.messageBag.title,
-                    message: this.arrayToList(this.messageBag.text),
-                    type: this.messageBag.type,
-                    dangerouslyUseHTMLString: true,
-                });
-            });
-        },
-        methods: {
-            arrayToList: (messages) => {
-                if (!Array.isArray(messages)) {
-                    return messages;
-                }
-                let result = '<ul>';
-                messages.forEach(message => {
-                    result += `<li>${message}</li>`;
-                });
-                result += '</ul>';
-                return result;
-            }
+      props: {
+        messageBag: {}
+      },
+      mounted () {
+        window.addEventListener('load', event => {
+          this.$notify({
+            title: this.messageBag.title,
+            message: this.arrayToList(this.messageBag.text),
+            type: this.messageBag.type,
+            dangerouslyUseHTMLString: true
+          });
+        });
+      },
+      methods: {
+        arrayToList: messages => {
+          if (!Array.isArray(messages)) {
+            return messages;
+          }
+          messages = messages.map(message => `<li>${message}</li>`);
+
+          return ['<ul>', ...messages, '</ul>'].join('');
         }
+      }
     });
 </script>
 
